554b51ffdd53c39b8034d5211eebd35f69635c77
Arman <argin9599@gmail.com>
2023-06-13 19:21:28 +0600

    final dark icon android

--- lib/main.dart
+++ lib/main.dart
@@ -20,38 +20,41 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[850],
+
       body: Center(
-          child: Container(
-        width: 200,
-        height: 200,
-        child: Icon(
-          Icons.android,
-          size: 80,
-          color: Colors.white,
+        child: Container(
+          width: 200,
+          height: 200,
+          child: Icon(
+            Icons.android,
+            size: 80,
+            color: Colors.white,
+          ),
+          decoration: BoxDecoration(
+            color: Colors.grey[850],
+            borderRadius: const BorderRadius.all(Radius.circular(50)),
+            boxShadow: [
+              BoxShadow(
+                color:  Colors.grey.shade900,
+                offset: const Offset(4.0, 4.0),
+                blurRadius: 15.0,
+                spreadRadius: 1.0,
+              ),
+              BoxShadow(
+                color: Colors.grey.shade800,
+                offset: const Offset(-4.0, -4.0),
+                blurRadius: 15.0,
+                spreadRadius: 1.0,
+              ),
+            ],
+          ),
         ),
-        decoration: BoxDecoration(
-          color: Colors.grey[850],
-          borderRadius: BorderRadius.all(Radius.circular(50)),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey,
-              offset: Offset(4.0, 4.0),
-              blurRadius: 15.0,
-              spreadRadius: 1.0
-            ),
-            BoxShadow(
-              color: Colors.white,
-              offset: Offset(-4.0, -4.0),
-              blurRadius: 15.0,
-              spreadRadius: 1.0
-            ),
-          ],
-        ),
-      )),
+      ),
     );
   }
 }
