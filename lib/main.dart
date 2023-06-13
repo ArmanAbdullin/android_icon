f7e1b305376e8b32e71f11b3e179e54b10f1561a
Arman <argin9599@gmail.com>
2023-06-13 18:47:00 +0600

    создал новый проект для создание иконки мягкой видимости

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Center(
+          child: Container(
+        width: 200,
+        height: 200,
+        child: Icon(
+          Icons.android,
+          size: 80,
+        ),
+      )),
+    );
+  }
+}
