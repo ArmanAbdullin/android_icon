6bba541d970a5ca086d0b8f7f8411545d08acbb1
Arman <argin9599@gmail.com>
2023-06-13 20:09:05 +0600

    add all change theme dark and white

--- lib/main.dart
+++ lib/main.dart
@@ -20,39 +20,83 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool darkMode = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[850],
-
+      backgroundColor: darkMode ? Colors.grey.shade800 : Colors.grey[300],
       body: Center(
-        child: Container(
-          width: 200,
-          height: 200,
-          child: Icon(
-            Icons.android,
-            size: 80,
-            color: Colors.white,
-          ),
-          decoration: BoxDecoration(
-            color: Colors.grey[850],
-            borderRadius: const BorderRadius.all(Radius.circular(50)),
-            boxShadow: [
-              BoxShadow(
-                color:  Colors.grey.shade900,
-                offset: const Offset(4.0, 4.0),
-                blurRadius: 15.0,
-                spreadRadius: 1.0,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              width: 200,
+              height: 200,
+              child: Icon(Icons.android,
+                  size: 80, color: darkMode ? Colors.white : Colors.black),
+              decoration: BoxDecoration(
+                color: darkMode ? Colors.grey.shade800 : Colors.grey[300],
+                borderRadius: const BorderRadius.all(Radius.circular(50)),
+                boxShadow: [
+                  BoxShadow(
+                    color: darkMode
+                        ? Colors.grey.shade900
+                        : Colors.grey.shade500, //Colors.grey.shade500,
+                    offset: const Offset(4.0, 4.0),
+                    blurRadius: 15.0,
+                    spreadRadius: 1.0,
+                  ),
+                  BoxShadow(
+                    color: darkMode
+                        ? Colors.grey.shade800
+                        : Colors.white, //Colors.white,
+                    offset: Offset(-4.0, -4.0),
+                    blurRadius: 15.0,
+                    spreadRadius: 1.0,
+                  ),
+                ],
               ),
-              BoxShadow(
-                color: Colors.grey.shade800,
-                offset: const Offset(-4.0, -4.0),
-                blurRadius: 15.0,
-                spreadRadius: 1.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 50),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    color: Colors.black,
+                    margin: EdgeInsets.only(top: 20.0),
+                    child: TextButton(
+                      onPressed: () {
+                        setState(() {
+                          darkMode = true;
+                        });
+                      },
+                      child: const Text(
+                        'Dark',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    color: Colors.white,
+                    margin: EdgeInsets.only(top: 20.0),
+                    child: TextButton(
+                      onPressed: () {
+                        setState(() {
+                          darkMode = false;
+                        });
+                      },
+                      child: const Text(
+                        'Light',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
